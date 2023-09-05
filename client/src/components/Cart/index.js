d09340c04cad462809905d6628370bd9585e42bb
Kateryna Stetsenko <itcreativeusa@gmail.com>
2023-09-05 12:12:50 -0700

    add import redux to cart component

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,7 +5,8 @@ import { QUERY_CHECKOUT } from "../../utils/queries";
 import { idbPromise } from "../../utils/helpers";
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
-import { useStoreContext } from "../../utils/GlobalState";
+import { useSelector, useDispatch } from "react-redux";
+
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import "./style.css";
 

a89d2089b6077c9e6128891194483704a2624ba0
Kateryna Stetsenko <itcreativeusa@gmail.com>
2023-09-06 12:10:39 -0700

    add commits to code

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,4 +1,5 @@
+// store is passed to the Provider component as a prop.
 import { createStore } from "redux";
 import reducer from "./reducers";
-
+// create store:
 export default createStore(reducer);
